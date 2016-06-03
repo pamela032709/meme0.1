295c7327b7fbf9d00a3ba156f5923631e1f51512
POCH <giselecheverri@poch-macbook-pro.local>
2016-06-03 12:13:59 -0400

    meme

--- meme0.1/ViewController.swift
+++ meme0.1/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
 
+    @IBOutlet weak var imagePickerView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject])
+    {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imagePickerView.image = image
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    
+    @IBAction func picker(sender: AnyObject) {
+        
+        var imagePick=UIImagePickerController()
+        
+        imagePick.delegate = self
+        imagePick.sourceType = .PhotoLibrary
+        self.presentViewController(imagePick, animated: true, completion: nil)
+        
+        
+        }
+   
+  
+    @IBAction func pickAnImageFromAlbum (sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
-
-
+    
+    
+    @IBAction func pickAnImageFromCamera (sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+    
+    }
 }
 
