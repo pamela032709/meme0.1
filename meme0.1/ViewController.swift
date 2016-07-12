1a3ae7eaeae0f41bfa6f0ac2451885d71e34e796
pamela032709 <giselecheverri@POCH-MacBook-Pro.local>
2016-07-11 23:23:52 -0400

    last update ,  share and cancel functions
    how to save the meme and share to social media

--- meme0.1/ViewController.swift
+++ meme0.1/ViewController.swift
@@ -12,10 +12,15 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
 
   
     @IBOutlet weak var ImagePickerView: UIImageView!
+    
+    
   
     
     @IBOutlet weak var topField: UITextField!
-    
+    //navigation bar
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    //share and cancel button
     @IBOutlet weak var cancelButton: UIBarButtonItem!
     @IBOutlet weak var shareButton: UIBarButtonItem!
     
@@ -45,18 +50,51 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         
         bottomField.defaultTextAttributes=memeTextAttributes
         bottomField.textAlignment = NSTextAlignment.Center}
+    
+        //shareButton.enabled = false
+    
+    
+    
+  //MOVE THE TEXT OUT OF THE PIC
+    func keyboardWillShow( notification: NSNotification){
+    self.view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    
+    func getKeyboardHeight(notification:NSNotification)->CGFloat {
+    let userinfo=notification.userInfo
+    let keyboardSize=userinfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+    return keyboardSize.CGRectValue().height
+    
+    }
+    
+    func subscribeToKeyboardNotification () {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotification(){
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    
+    }
+    
 
-    
+ //will get the pics from the album
 @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
-        let nextController  = UIImagePickerController()
-        self.presentViewController(nextController, animated: true, completion: nil)    }
+        let imagePicker  = UIImagePickerController()
+    imagePicker.delegate = self
+    imagePicker.sourceType=UIImagePickerControllerSourceType.PhotoLibrary
+    presentViewController(imagePicker, animated: true, completion: nil)
     
+    }
     
     //method will get pics from the camera directly
     
     @IBAction func pickAnImageFromCamera (sender: AnyObject) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        imagePicker.sourceType=UIImagePickerControllerSourceType.Camera
         presentViewController(imagePicker, animated: true, completion: nil)
     }
     
@@ -65,28 +103,69 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
        print("viewWillAppearnow")
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
             super.viewWillAppear(animated)
-            // Subscribe to keyboard notifications to allow the view to raise when necessary
+       
         
-        }
+        
+            // Subscribe to keyboard notifications to allow the view to raise when necessary
+    }
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear (animated)
+        self.unsubscribeFromKeyboardNotification()
+    }
+    //dismiss keyboard
     
-    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
     
     //retrieve images
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        if (info[UIImagePickerControllerOriginalImage] as? UIImage) != nil {
-           // imageViewController.image = image
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        
+        ImagePickerView.image = image
+        
             self.dismissViewControllerAnimated(true, completion: nil)
         }
-    }
+    
  
-  
-    
-    
-    
-    
+
         }
    
+    func save1() {
+        
+        // Create the Meme object
+        var myMeme = meme(topField: topField.text!, bottomField: bottomField.text!, image: ImagePickerView.image!, memedImage: generateMemedImage())
+        // Add it to the Meme array
+        (UIApplication.sharedApplication().delegate as! AppDelegate).memes.append(meme)
+    }
 
+    func generateMemedImage () -> UIImage {
+        
+        // Hide navigation bar & tool bar
+        
+        toolBar.hidden = true
+        navigationBar.hidden = true
+        
+        // Resign first responders , we don't want to capture the editing blue line if we are still in the text field when generating a Meme
+        topField.resignFirstResponder()
+        bottomField.resignFirstResponder()
+        
+        //Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        self.view.drawViewHierarchyInRect(self.view.frame, afterScreenUpdates: true)
+        let memedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        //Show navigation bar & tool bar
+        toolBar.hidden = false
+        navigationBar.hidden = false
+        
+        return memedImage
+        
+    }
+}
 
 
